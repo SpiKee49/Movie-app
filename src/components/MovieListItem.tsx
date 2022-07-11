3885ee6748770942e3bb41487500d493e31ae512
René Bukovina <r.bukovina@gmail.com>
2022-07-11 11:24:27 +0200

    design changes

--- src/components/MovieListItem.tsx
+++ src/components/MovieListItem.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Movie } from '../app/types';
 import { useNavigate } from 'react-router-dom';
-import { ImageNotFoundIcon } from './Icons';
+import { FavoriteIcon, ImageNotFoundIcon } from './Icons';
+import { useAppSelector } from '../app/hooks';
 
 function MovieListItem({ movie }: { movie: Movie }) {
   const navigate = useNavigate();
+
+  const { favorites } = useAppSelector(
+    store => store.movies
+  );
+
+  const isFavorite = [...favorites]
+    .map(item => item.imdbID)
+    .includes(movie.imdbID);
+
+  const rating = isNaN(+movie.imdbRating)
+    ? ''
+    : +movie.imdbRating * 10 + '%';
 
   return (
     <tr
@@ -17,8 +30,16 @@ function MovieListItem({ movie }: { movie: Movie }) {
       <td className="description">
         {movie.Title}
         <div className="info">
-          {`Type: ${movie.Type} / Year: ${movie.Year} / Director: ${movie.Director} / Actors: ${movie.Actors} / Runtime: ${movie.Runtime}`}
+          {`Type: ${movie.Type} / Year: ${movie.Year} / Genre: ${movie.Genre}`}
         </div>
+      </td>
+      <td className="rating">
+        <p>
+          <span>{rating}</span>
+        </p>
+      </td>
+      <td className="favorite">
+        {isFavorite && <FavoriteIcon value={true} />}
       </td>
     </tr>
   );
